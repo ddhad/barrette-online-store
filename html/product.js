f321541987133159162f44fa0490d5c3c88a8e87
ddhad <d_had@outlook.com>
2023-07-21 18:23:43 +0300

    Add download script and build new db, adjust endpoints

--- html/product.js
+++ html/product.js
@@ -18,9 +18,7 @@ const mainImg = document.getElementById("imageBox");
 const secondaryImgs = document.getElementById("secondaryImageBox");
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log(secondaryImgs)
-  console.log(mainImg)
-  mainImg.setAttribute("src", "data:image/png;base64," + productImgs[0]);
+  mainImg.setAttribute("src", "data:image/png;base64," + productImgs);
   let data = "";
   productImgs.forEach((imgSrc) => {
     let imgEl = document.createElement("a");
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 
-DB_NAME = "example.db"
+DB_NAME = "db/barrette.db"
 
 templates = Jinja2Templates(directory="html")
 app = FastAPI()
@@ -37,18 +37,21 @@ def get_product(item_id: int, request: Request):
         return {'ERROR': 'PRODUCT NOT FOUND'}
     product = {
         "id": item[0],
-        "name": item[1],
-        "price": item[2],
-        "category": item[3],
-        "series": item[4],
-        "series_description": item[5],
-        "brand": item[6],
-        "quantity": item[7],
-        "description": item[8],
-        "usage": item[9],
-        "ingredients": item[10],
+        "barcode": item[1],
+        "bg_name": item[2],
+        "en_name": item[3],
+        "material_code": item[4],
+        "series": item[5],
+        "series_description": item[6],
+        "product_type": item[7],
+        "short_description": item[8],
+        "description": item[9],
+        "usage": item[10],
+        "ingredients": item[11],
+        "benefits": item[12],
+        "size": item[13],
         "images": json.dumps([
-                base64.b64encode(img).decode('utf-8') for img in eval(item[11])
+                base64.b64encode(img).decode('utf-8') for img in eval(item[14])
             ])
     }
     return templates.TemplateResponse(
@@ -66,20 +69,28 @@ def get_home_page_(request: Request):
     con.close()
 
     products = []
-    for item in items:
+    for item in items[0:8]:
+        image = eval(item[14])
+        if len(image) > 0:
+            image = base64.b64encode(image[0]).decode('utf-8')
+        else:
+            image = None
         product = {
             "id": item[0],
-            "name": item[1],
-            "price": item[2],
-            "category": item[3],
-            "series": item[4],
-            "series_description": item[5],
-            "brand": item[6],
-            "quantity": item[7],
-            "description": item[8],
-            "usage": item[9],
-            "ingredients": item[10],
-            "image": base64.b64encode(eval(item[11])[0]).decode('utf-8')
+            "barcode": item[1],
+            "bg_name": item[2],
+            "en_name": item[3],
+            "material_code": item[4],
+            "series": item[5],
+            "series_description": item[6],
+            "product_type": item[7],
+            "short_description": item[8],
+            "description": item[9],
+            "usage": item[10],
+            "ingredients": item[11],
+            "benefits": item[12],
+            "size": item[13],
+            "image": image
         }
         products.append(product)
     return templates.TemplateResponse(
